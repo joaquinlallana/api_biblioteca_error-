2bf3e581ad407958da550188003ec947384ff788
joaquinlallana <joaquinlallana12@gmail.com>
2024-01-23 23:14:28 -0300

    Tests Unitarios y de Integración.

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,15 +1,25 @@
-const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_DB, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
+/*
+mongoose.connect("mongodb://127.0.0.1:27017/biblioteca", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+*/
+const LibroSchema = new mongoose.Schema(
+  {
+    titulo: String,
+    autor: String,
+  },
+  { collection: "libros" }
+);
 
-const LibroSchema = new mongoose.Schema({
-  titulo: String,
-  autor: String
-}, { collection: 'libros' });
-
-const Libro = mongoose.model('Libro', LibroSchema);
+const Libro = mongoose.model("Libro", LibroSchema);
 
 module.exports = Libro;
